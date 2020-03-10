1fe0250b8b63d2e8433e2c7b1f2c9b685d85441e
Kobi Gurkan <kobigurk@gmail.com>
2020-03-10 19:44:10 +0200

    updates example to celo

--- basic-node-hid/src/index.js
+++ basic-node-hid/src/index.js
@@ -1,13 +1,14 @@
 import "babel-polyfill";
 import TransportNodeHid from "@ledgerhq/hw-transport-node-hid";
-import AppBtc from "@ledgerhq/hw-app-btc";
+import AppEth from "@ledgerhq/hw-app-eth";
 
 async function example() {
   const transport = await TransportNodeHid.open("");
   transport.setDebugMode(true);
-  const appBtc = new AppBtc(transport);
-  const result = await appBtc.getWalletPublicKey("44'/0'/0'/0/0");
-  return result;
+  const appEth = new AppEth(transport);
+  const result = await appEth.getAddress("44'/52752'/0'/0/0");
+  const result2 = await appEth.signTransaction("44'/52752'/0'/0/0", "f840050782520880943254cf5b6509a634fdaec8d9bc0656ede78f8e7d821388943254cf5b6509a634fdaec8d9bc0656ede78f8e7d884563918244f4000080808080");
+  return [result, result2];
 }
 
 example().then(
